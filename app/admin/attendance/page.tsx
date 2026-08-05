997ddd740c58baf4a40458f2d4d28cf330569248
Apartment218 <brighttya09@gmail.com>
2026-08-05 16:58:00 +0100

    Make attendance page dynamic

--- app/admin/attendance/page.tsx
+++ app/admin/attendance/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 const dateTimeFormatter = new Intl.DateTimeFormat("en-GB", {
   dateStyle: "medium",
   timeStyle: "short",
